a516d8c5a48f9483848ea64e06eed8f20d457283
Mizzery Solomon <solomonmg.882.stud@cdd.edu.ph>
2024-07-18 12:24:49 +0800

    Laravel Model Controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory(20)->create()([
+            //'name' => 'Test User',
+            //'email' => 'test@example.com',
         ]);
     }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+ //Route::get('/comment_replies', function () {
+    return view('comment_replies');
+    //});
+ //Route::get('/post_comments', function () {
+    return view('post_comments');
+   // });
+ //Route::get('/posts', function () {
+    return view('posts');
+   // });
+
+//Route::resource('post_comments', [Controllers::class, 'index']);
+//Route::resource('comments_replies', [Controllers::class, 'create']);
+//Route::resource('posts', [Controllers::class, 'index']);
+
+//Route::resource('posts', Controllers::class);
+//Route::resource('post_comments', Controllers::class);
+
+Route::resource('comment_replies', Controllers::class);
